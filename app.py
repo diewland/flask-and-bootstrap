e28a7f1a8e562a1199de5240ffa19995bffdb0b6
yuwat <yuwat@mfec.co.th>
2019-10-31 20:18:33 +0700

    add template

--- app.py
+++ app.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html', title='Index Screen')
+    return render_template('home.html')
+
+
+@app.route('/one', methods=['GET'])
+def one():
+    return render_template('one.html')
+
+
+@app.route('/two', methods=['GET'])
+def two():
+    return render_template('two.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
